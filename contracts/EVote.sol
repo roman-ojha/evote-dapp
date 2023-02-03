1ed29601860153cebfa06872599cedc300099f6a
Roman-Ojha <razzroman98@gmail.com>
2023-02-03 11:34:01 +0545

    feat: fun to create new election

--- contracts/EVote.sol
+++ contracts/EVote.sol
@@ -1,27 +1,40 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.9.0;
-
-struct Admin {
-    address payable id;
-}
-
-enum user {
-    candidate,
-    voter
+enum Role {
+    admin,
+    user
 }
 struct User {
     address id;
-    user user_type;
+    Role user_type;
     bool does_exist;
 }
 
+struct Election {
+    uint256 id;
+    string name;
+    string desc;
+    bool is_finished;
+    uint256 created_at;
+    uint256 start_at;
+    uint256 end_at;
+    uint256 no_of_voters;
+    mapping(address => User) candidate;
+}
+
 contract EVote {
-    Admin admin;
     mapping(address => User) users;
+    uint256 no_of_election;
+    mapping(uint256 => Election) elections;
 
     constructor() {
-        admin.id = payable(msg.sender);
+        User memory admin = User({
+            id: msg.sender,
+            user_type: Role.admin,
+            does_exist: true
+        });
+        users[msg.sender] = admin;
     }
 
     // Authentication
@@ -34,7 +47,7 @@ contract EVote {
     }
     modifier isAdmin() {
         require(
-            admin.id == msg.sender,
+            users[msg.sender].user_type == Role.admin,
             "unauthorized admin, you are not an admin"
         );
         _;
@@ -45,7 +58,7 @@ contract EVote {
             // if user doesn't exist already
             User memory new_user = User({
                 id: msg.sender,
-                user_type: user.voter,
+                user_type: Role.user,
                 does_exist: true
             });
             users[msg.sender] = new_user;
@@ -59,15 +72,45 @@ contract EVote {
     function candidate_form() public payable isUser {
         // can access by normal user to request for candidate
         // for that user have to send '0.1' eth
-        users[msg.sender].user_type = user.candidate;
+        // users[msg.sender].user_type = user.candidate;
     }
 
     function am_I_a_candidate() public view isUser returns (bool) {
-        if (users[msg.sender].user_type == user.candidate) {
-            return true;
-        }
-        return false;
+        // if (users[msg.sender].user_type == user.candidate) {
+        //     return true;
+        // }
+        // return false;
     }
 
-    function add_candidate() public view {}
+    function create_new_election(
+        string memory _name,
+        string memory _desc,
+        uint256 _start_at_in_days,
+        uint256 _end_at_in_days
+    ) public isAdmin {
+        no_of_election++;
+        uint256 _start_at = _start_at_in_days * 24 * 60 * 60;
+        uint256 _end_at = _end_at_in_days * 25 * 60 * 60;
+
+        // Election memory new_election = Election({
+        //     id: no_of_election,
+        //     name: _name,
+        //     desc: _desc,
+        //     is_finished: false,
+        //     created_at: block.timestamp,
+        //     start_at: _start_at,
+        //     end_at: _end_at
+        //     no_of_voters:0,
+        // });
+
+        Election storage new_election = elections[no_of_election];
+        new_election.id = no_of_election;
+        new_election.name = _name;
+        new_election.desc = _desc;
+        new_election.is_finished = false;
+        new_election.created_at = block.timestamp;
+        new_election.start_at = _start_at;
+        new_election.end_at = _end_at;
+        new_election.no_of_voters = 0;
+    }
 }
