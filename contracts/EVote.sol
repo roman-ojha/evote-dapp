b7b3e0e732601f99ff1c53c6ae83bf75b7ee3093
Roman-Ojha <razzroman98@gmail.com>
2023-02-03 10:42:45 +0545

    feat: added login & get_user function on EVote contract

--- contracts/EVote.sol
+++ contracts/EVote.sol
@@ -13,16 +13,31 @@ enum user {
 struct User {
     address id;
     user user_type;
+    bool does_exist;
 }
 
 contract EVote {
     Admin admin;
+    mapping(address => User) users;
 
     constructor() {
         admin.id = payable(msg.sender);
     }
 
-    // function register() public view {
-    //     User memory new_user = User({id: msg.sender, user_type: user.voter});
-    // }
+    function login() public {
+        if (!users[msg.sender].does_exist) {
+            // if user doesn't exist already
+            User memory new_user = User({
+                id: msg.sender,
+                user_type: user.voter,
+                does_exist: true
+            });
+            users[msg.sender] = new_user;
+        }
+    }
+
+    function get_user() public view returns (User memory) {
+        require(users[msg.sender].does_exist, "User doesn't exist");
+        return users[msg.sender];
+    }
 }
