d224bf9334db3f338f1da70ec0f551330938ef54
Roman-Ojha <razzroman98@gmail.com>
2023-02-03 13:34:25 +0545

    feat: fun to create candidate form & transfer balance to admin

--- contracts/EVote.sol
+++ contracts/EVote.sol
@@ -14,6 +14,7 @@ struct User {
 struct Candidate {
     address id;
     string agenda;
+    bool does_exist;
 }
 
 struct Voter {
@@ -30,24 +31,27 @@ struct Election {
     uint256 end_at;
     uint256 no_of_voters;
     uint256 no_of_candidates;
-    Candidate[] candidates;
-    Voter[] voters;
-    // mapping(address => Candidate) candidates;
-    // mapping(address => Voter) voters;
+    // Candidate[] candidates;
+    // Voter[] voters;
+    mapping(address => Candidate) candidates;
+    mapping(address => Voter) voters;
 }
 
 contract EVote {
+    address payable admin;
     mapping(address => User) users;
     // mapping(uint256 => Election) elections;
     Election[] elections;
+    uint256 public candidate_form_fee = 100 wei;
 
     constructor() {
-        User memory admin = User({
+        User memory new_admin = User({
             id: msg.sender,
             user_type: Role.admin,
             does_exist: true
         });
-        users[msg.sender] = admin;
+        users[msg.sender] = new_admin;
+        admin = payable(msg.sender);
     }
 
     // Authentication
@@ -82,29 +86,19 @@ contract EVote {
         return users[msg.sender];
     }
 
-    function candidate_form() public payable isUser {
-        // can access by normal user to request for candidate
-        // for that user have to send '0.1' eth
-        // users[msg.sender].user_type = user.candidate;
-    }
-
-    function am_I_a_candidate() public view isUser returns (bool) {
-        // if (users[msg.sender].user_type == user.candidate) {
-        //     return true;
-        // }
-        // return false;
-    }
-
     function create_new_election(
         string memory _name,
         string memory _desc,
-        uint256 _start_at_in_days,
-        uint256 _end_at_in_days
+        uint256 _start_after_in_days,
+        uint256 _end_after_in_days
     ) public isAdmin returns (uint256 _id) {
         // returns: id
 
-        uint256 _start_at = _start_at_in_days * 24 * 60 * 60;
-        uint256 _end_at = _end_at_in_days * 25 * 60 * 60;
+        uint256 _start_at = _start_after_in_days * 24 * 60 * 60;
+        uint256 _end_at = (_start_after_in_days + _end_after_in_days) *
+            25 *
+            60 *
+            60;
 
         // Election memory new_election = Election({
         //     id: elections.length,
@@ -119,7 +113,8 @@ contract EVote {
         //     voters: new address[](7)
         // });
         uint256 id = elections.length;
-        Election memory new_election;
+        elections.push();
+        Election storage new_election = elections[id];
         new_election.id = id;
         new_election.name = _name;
         new_election.desc = _desc;
@@ -129,7 +124,6 @@ contract EVote {
         new_election.end_at = _end_at;
         new_election.no_of_voters = 0;
         new_election.no_of_candidates = 0;
-        elections.push(new_election);
         return id;
     }
 
@@ -144,9 +138,7 @@ contract EVote {
             uint256 start_at,
             uint256 end_at,
             uint256 no_of_voters,
-            uint256 no_of_candidates,
-            Candidate[] memory candidates,
-            Voter[] memory voters
+            uint256 no_of_candidates
         )
     {
         Election storage election = elections[_id];
@@ -158,9 +150,49 @@ contract EVote {
             election.start_at,
             election.end_at,
             election.no_of_voters,
-            election.no_of_candidates,
-            election.candidates,
-            election.voters
+            election.no_of_candidates
         );
+    }
+
+    function candidate_form(uint256 _election_id, string memory _agenda)
+        public
+        payable
+        isUser
+    {
+        // can access by normal user to request for candidate
+        // for that user have to send '0.1' eth
+        require(
+            msg.value >= candidate_form_fee,
+            "Please submit with at least 100 wei"
+        );
+        Candidate memory candidate = Candidate({
+            id: msg.sender,
+            agenda: _agenda,
+            does_exist: true
+        });
+        elections[_election_id].candidates[msg.sender] = candidate;
+    }
+
+    function am_I_a_candidate(uint256 _election_id)
+        public
+        view
+        isUser
+        returns (bool)
+    {
+        if (elections[_election_id].candidates[msg.sender].does_exist) {
+            return true;
+        }
+        return false;
+    }
+
+    // function to transfer balance from contract to admin
+    function transfer_balance_to_admin() public isAdmin {
+        uint256 contract_balance = address(this).balance;
+        admin.transfer(contract_balance);
+    }
+
+    // function to get admin balance
+    function get_admin_balance() public view isAdmin returns (uint256) {
+        return address(msg.sender).balance;
     }
 }
