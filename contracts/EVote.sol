ee1090bf31d1708ad7f98226140b5a616d555cf7
Roman-Ojha <razzroman98@gmail.com>
2023-02-03 10:16:03 +0545

    feat: defined basic Admin & User types

--- contracts/EVote.sol
+++ contracts/EVote.sol
@@ -2,8 +2,27 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+struct Admin {
+    address payable id;
+}
+
+enum user {
+    candidate,
+    voter
+}
+struct User {
+    address id;
+    user user_type;
+}
+
 contract EVote {
-    function hello() public pure returns (string memory) {
-        return "Hello world";
+    Admin admin;
+
+    constructor() {
+        admin.id = payable(msg.sender);
     }
+
+    // function register() public view {
+    //     User memory new_user = User({id: msg.sender, user_type: user.voter});
+    // }
 }
